dce4f1bd05713c72519c25d0ad0a281451bf6797
nmt2502 <anhlatuan2502@gmail.com>
2026-01-20 02:44:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ require("./sun")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server By Bi Nhoi Chạy Tại, PORT);
 });
